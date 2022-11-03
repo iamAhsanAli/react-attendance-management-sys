2cca442bdf914a1601a877236a40785947ccaf81
Ahsan <ahsan49394939@gmail.com>
2022-11-03 16:10:25 +0500

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,36 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'ahsan',
+    lastName: 'ali',
+    email: 'ahsan@example.com',
+    date: '2019-04-11'
+    
+  },
+  {
+    id: 2,
+    firstName: 'afaq',
+    lastName: 'ali',
+    email: 'afaq@example.com',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'shaheen',
+    lastName: 'zafar',
+    email: 'shaheen@example.com',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'aliyan',
+    lastName: 'malik',
+    email: 'malik@example.com',
+    date: '2019-05-03'
+  },
+ 
+
+ 
+];
+
+export { employeesData };
